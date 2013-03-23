04e6d2e98916071642bef9c19f459004c29f2002
bob <bobthekingofegypt@googlemail.com>
2013-03-23 22:19:38 +0000

    added type validation to card model

--- bobswitch/engine/models.py
+++ bobswitch/engine/models.py
@@ -1,4 +1,25 @@
+from flufl.enum import Enum
 
+class Suit(Enum):
+    hearts = 0
+    clubs = 1
+    diamonds = 2
+    spades = 3
+
+class Rank(Enum):
+    ace = 1
+    two = 2
+    three = 3
+    four = 4
+    five = 5
+    six = 6
+    seven = 7
+    eight = 8
+    nine = 9
+    ten = 10
+    jack = 11
+    queen = 12
+    king = 13
 
 class Room(object):
     """
@@ -12,6 +33,11 @@ class Card(object):
     represents a card
     """
     def __init__(self, suit, rank):
+        if suit not in Suit:
+            raise TypeError("suit argument must be off type Suit")
+        if rank not in Rank:
+            raise TypeError("rank argument must be off type Rank")
+
         self.suit = suit
         self.rank = rank
 
@@ -19,7 +45,8 @@ class Deck(object):
     """
     represents the cards still to play and their order
     """
-    pass
+    def banana(self):
+        print "test"
 
 class Hand(object):
     """
--- bobswitch/tests/test_models.py
+++ bobswitch/tests/test_models.py
@@ -4,7 +4,19 @@ from engine import models
 
 class TestCard(TestCase):
     
-    def test_init(self):
-        card = models.Card() 
-        self.assertTrue(True)
+    def test_init_valid(self):
+        card = models.Card(models.Suit.hearts, models.Rank.ace) 
+
+        self.assertEquals(card.suit, models.Suit.hearts, 
+                "card should be a heart")
+        self.assertEquals(card.rank, models.Rank.ace, 
+                "card should be an ace")
+
+    def test_init_invalid_suit(self):
+        card = models.Card(models.Suit.hearts, models.Rank.ace) 
+        self.failUnlessRaises(TypeError, models.Card, 1, models.Rank.ace)
+
+    def test_init_invalid_rank(self):
+        card = models.Card(models.Suit.hearts, models.Rank.ace) 
+        self.failUnlessRaises(TypeError, models.Card, models.Suit.hearts, 1)
 
