e77e54519f32251e143aaa93dc421070663d69a1
bob <bobthekingofegypt@googlemail.com>
2013-03-24 20:32:54 +0000

    updated model to store hand

--- bobswitch/engine/models.py
+++ bobswitch/engine/models.py
@@ -30,9 +30,14 @@ class Room(object):
 
 class Card(object):
     """
-    represents a card
+    Represents a card from a standard deck of cards, eg- ace of clubs
     """
+
     def __init__(self, suit, rank):
+        """
+        suit and rank must be of the enum type Rank and Card, if they
+        are not a TypeError will be raised.
+        """
         if suit not in Suit:
             raise TypeError("suit argument must be off type Suit")
         if rank not in Rank:
@@ -41,6 +46,49 @@ class Card(object):
         self.suit = suit
         self.rank = rank
 
+    def __eq__(self, other):
+        return (self.suit == other.suit and self.rank == other.rank)
+
+    def __neq__(self, other):
+        return not self.__eq__(other)
+
+    def __repr__(self):
+        return "Card: suit - %r, rank - %r" % (self.suit.name, self.rank.name)
+
+
+class Hand(object):
+    """
+    Represents a set of cards 
+    """
+
+    def __init__(self):
+        self.cards = []
+
+
+    def add_card(self, card):
+        """
+        adds the given card to the hand
+        """
+        self.cards.append(card)
+
+    def contains_card(self, card):
+        """
+        Returns True if hand contains passed card
+        """
+        return card in self.cards
+
+    def remove_card(self, card):
+        """
+        Returns True if the card was removed from the hand, returns
+        False if the card was not present
+        """
+        if self.contains_card(card):
+            self.cards.remove(card)
+            return True
+        
+        return False
+
+
 class Deck(object):
     """
     represents the cards still to play and their order
@@ -48,11 +96,6 @@ class Deck(object):
     def banana(self):
         print "test"
 
-class Hand(object):
-    """
-    represents a set of cards the player is currently playing
-    """
-    pass
 
 class PlayedCards(object):
     """
--- bobswitch/tests/test_models.py
+++ bobswitch/tests/test_models.py
@@ -7,9 +7,9 @@ class TestCard(TestCase):
     def test_init_valid(self):
         card = models.Card(models.Suit.hearts, models.Rank.ace) 
 
-        self.assertEquals(card.suit, models.Suit.hearts, 
+        self.assertEquals(models.Suit.hearts, card.suit, 
                 "card should be a heart")
-        self.assertEquals(card.rank, models.Rank.ace, 
+        self.assertEquals(models.Rank.ace, card.rank,
                 "card should be an ace")
 
     def test_init_invalid_suit(self):
@@ -20,3 +20,76 @@ class TestCard(TestCase):
         card = models.Card(models.Suit.hearts, models.Rank.ace) 
         self.failUnlessRaises(TypeError, models.Card, models.Suit.hearts, 1)
 
+    def test_repr(self):
+        card = models.Card(models.Suit.hearts, models.Rank.ace) 
+        self.assertEquals("Card: suit - 'hearts', rank - 'ace'", card.__repr__())
+
+    def test_eq(self):
+        card = models.Card(models.Suit.hearts, models.Rank.ace) 
+        card2 = models.Card(models.Suit.hearts, models.Rank.ace) 
+
+        self.assertEquals(card, card2)
+
+    def test_neq_suit(self):
+        card = models.Card(models.Suit.hearts, models.Rank.ace) 
+        card2 = models.Card(models.Suit.clubs, models.Rank.ace) 
+
+        self.assertNotEquals(card, card2)
+    
+    def test_neq_rank(self):
+        card = models.Card(models.Suit.hearts, models.Rank.ace) 
+        card2 = models.Card(models.Suit.hearts, models.Rank.two) 
+
+        self.assertNotEquals(card, card2)
+    
+    def test_neq_both(self):
+        card = models.Card(models.Suit.hearts, models.Rank.ace) 
+        card2 = models.Card(models.Suit.spades, models.Rank.two) 
+
+        self.assertNotEquals(card, card2)
+
+
+class TestHand(TestCase):
+
+    def test_init(self):
+        hand = models.Hand()
+        self.assertEquals(0, len(hand.cards))
+        
+
+    def test_add_card(self):
+        hand = models.Hand()
+        hand.add_card(models.Card(models.Suit.hearts, models.Rank.ace))
+
+        self.assertEquals(1, len(hand.cards))
+
+    def test_contains(self):
+        hand = models.Hand()
+        hand.add_card(models.Card(models.Suit.hearts, models.Rank.ace))
+
+        card = models.Card(models.Suit.hearts, models.Rank.ace)
+        self.assertTrue(hand.contains_card(card))
+
+    def test_doesnt_contains(self):
+        hand = models.Hand()
+        hand.add_card(models.Card(models.Suit.hearts, models.Rank.ace))
+
+        card = models.Card(models.Suit.clubs, models.Rank.ace)
+        self.assertFalse(hand.contains_card(card))
+
+    def test_remove_card(self):
+        hand = models.Hand()
+        hand.add_card(models.Card(models.Suit.hearts, models.Rank.ace))
+
+        card = models.Card(models.Suit.hearts, models.Rank.ace)
+        result = hand.remove_card(card)
+        self.assertTrue(result)
+        self.assertEquals(0, len(hand.cards))
+
+    def test_remove_card_failure(self):
+        hand = models.Hand()
+        hand.add_card(models.Card(models.Suit.hearts, models.Rank.ace))
+
+        card = models.Card(models.Suit.clubs, models.Rank.ace)
+        result = hand.remove_card(card)
+        self.assertFalse(result)
+        self.assertEquals(1, len(hand.cards))
